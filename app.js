7885202fec552328608059b86437ca5aa6c5653a
Marco Mercado <Marco@Marcos-MBP.home>
2019-09-28 16:51:17 -0700

    first webpage render

---
+++ app.js
@@ -0,0 +1,19 @@
+let express = require('express')
+let path = require('path')
+let app = express()
+
+app.use(express.static("public"));
+
+app.set("view engine","ejs"); // so that we can rander ejs
+
+
+//configure app to serve static files from public folder
+app.use(express.static(path.join(__dirname, 'public')))
+app.get( "/",(req, res) => {
+  //respond with index.html 
+  console.log(__dirname)
+  res.render("main")
+})
+app.listen(3000 || process.env.PORT, () => {
+  console.log("Server listening on port 3000")
+})

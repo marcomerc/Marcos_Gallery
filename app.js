bea65001e8dae026f92beb2dd771764f2853d891
Marco Mercado <Marco@Marcos-MBP.home>
2019-09-30 18:20:44 -0700

    all the routes have been set up

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.set("view engine","ejs"); // so that we can rander ejs
 app.use(express.static(path.join(__dirname, 'public')))
 app.get( "/",(req, res) => {
   //respond with index.html 
-  console.log(__dirname)
   res.render("main")
 })
+
 
 app.get("/picture1",(req,res)=>{
 
@@ -22,24 +22,29 @@ app.get("/picture1",(req,res)=>{
 })
 app.get("/picture2",(req,res)=>{
 
-    res.render("pic1")
+    res.render("pic2")
 })
-app.get("/picture2",(req,res)=>{
+app.get("/picture3",(req,res)=>{
 
-    res.render("pic1")
+    res.render("pic3")
 })
-app.get("/picture2",(req,res)=>{
+app.get("/picture4",(req,res)=>{
 
-    res.render("pic1")
+    res.render("pic4")
 })
-app.get("/picture2",(req,res)=>{
+app.get("/picture5",(req,res)=>{
 
-    res.render("pic1")
+    res.render("pic5")
 })
-app.get("/picture2",(req,res)=>{
+app.get("/picture6",(req,res)=>{
 
-    res.render("pic1")
+    res.render("pic6")
 })
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+
+
 app.listen(port, () => {
   console.log("the port",process.env.PORT)
 })
